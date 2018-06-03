373af4ec408396b388c59fd223e2a1c2adb38bc9
Kevin Chappell <kevin.b.chappell@gmail.com>
2018-06-03 04:27:21 +0100

    Removes babel-regenerator-runtime, babel-preset-2015 switch to babel-minify code styled changes

--- src/mi18n.test.js
+++ src/mi18n.test.js
@@ -1,4 +1,3 @@
-require('babel-regenerator-runtime');
 const {expect} = require('chai');
 const mi18n = require('../dist/mi18n.min.js').default;
 
@@ -17,34 +16,35 @@ describe('I18N', () => {
     expect(mi18n).to.exist;
   });
 
-  describe('should have methods', async () => {
-    await mi18n.init(testOpts);
-    describe('init()', () => {
-      it('should exist', () => {
-        expect(mi18n.init).to.exist;
-        expect(typeof mi18n.init).to.equal('function');
-      });
-      it('should set locale', () => {
-        expect(mi18n.config.locale).to.equal(locale);
-      });
-      it('should set current language', () => {
-        expect(mi18n.current).to.exist;
-        expect(Object.keys(mi18n.current)).to.contain('testKey');
+  describe('should have methods', () => {
+    mi18n.init(testOpts).then(() => {
+      describe('init()', () => {
+        it('should exist', () => {
+          expect(mi18n.init).to.exist;
+          expect(typeof mi18n.init).to.equal('function');
+        });
+        it('should set locale', () => {
+          expect(mi18n.config.locale).to.equal(locale);
+        });
+        it('should set current language', () => {
+          expect(mi18n.current).to.exist;
+          expect(Object.keys(mi18n.current)).to.contain('testKey');
+        });
       });
     });
   });
 
-  describe('should have methods after init', async () => {
-    await mi18n.init(testOpts);
+  describe('should have methods after init', () => {
+    mi18n.init(testOpts).then(() => {
+      describe('get()', () => {
+        it('shall exist', () =>
+          expect(mi18n.get).to.exist
+        );
 
-    describe('get()', () => {
-      it('shall exist', () =>
-        expect(mi18n.get).to.exist
-      );
-
-      it('shall return a string', () => {
-        const str = mi18n.get('testKey');
-        expect(str).to.equal('Teeesst');
+        it('shall return a string', () => {
+          const str = mi18n.get('testKey');
+          expect(str).to.equal('Teeesst');
+        });
       });
     });
   });
